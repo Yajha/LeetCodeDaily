38786a18df0126fb28e6db0e969d762de71a17b3
yajha <seuoplus@gmail.com>
2022-04-30 22:09:59 +0800

    2022-04-30

---
+++ Code/173/P173.java
@@ -0,0 +1,41 @@
+package yajha.solution;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class P173 {
+
+    List<Integer> res = new LinkedList<>();
+    int idx=-1;
+    public P173(TreeNode root) {
+        inOrderTraversal(root);
+        System.out.println(res.toString());
+    }
+
+    public int next() {
+        idx+=1;
+        return res.get(idx);
+    }
+
+    public boolean hasNext() {
+        return idx<res.size();
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        Deque<TreeNode> deque=new ArrayDeque<>();
+        TreeNode node=root;
+        while (!deque.isEmpty() || node!=null) {
+            while (node != null) {
+                deque.offerLast(node);
+                node = node.left;
+            }
+            node=deque.removeLast();
+            res.add(node.val);
+            node=node.right;
+
+        }
+    }
+
+}
